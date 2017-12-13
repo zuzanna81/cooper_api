463362dd8cf61fab2dc98e06f64b370207c04d9a
Zuzanna <zuzanna.bienkowska@gmail.com>
2017-12-13 20:02:48 +0100

    Add controller for historical data Add route Add create method in the controller Add index method in the controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
     namespace :v0 do
       resources :ping, only: [:index], constraints: { format: 'json' }
     end
-    namespace :v1 do
+    namespace :v1, defaults: { format: :json } do
       mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
+      resources :performance_data, only: [:create, :index] 
     end
   end
 end
