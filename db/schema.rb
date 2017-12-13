9ece81fd487c461383232a3e38a2a6b0c45c8c35
Zuzanna <zuzanna.bienkowska@gmail.com>
2017-12-13 19:43:29 +0100

    Add Performance Data model to store historical data

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171213114730) do
+ActiveRecord::Schema.define(version: 20171213183622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
+
+  create_table "performance_data", force: :cascade do |t|
+    t.integer  "user_id"
+    t.hstore   "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_performance_data_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "provider",               default: "email", null: false
@@ -44,4 +53,5 @@ ActiveRecord::Schema.define(version: 20171213114730) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
   end
 
+  add_foreign_key "performance_data", "users"
 end
