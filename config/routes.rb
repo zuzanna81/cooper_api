53bfebb5ebb7eefbff1ffab1510fad586f9ae35a
Zuzanna <zuzanna.bienkowska@gmail.com>
2017-12-13 13:44:04 +0100

    Add new namespace to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
     namespace :v0 do
       resources :ping, only: [:index], constraints: { format: 'json' }
     end
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
+    end
   end
 end
