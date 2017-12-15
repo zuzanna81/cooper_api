0b105e40bd5d066988331266a98151a748e2421f
Zuzanna <zuzanna.bienkowska@gmail.com>
2017-12-15 10:28:52 +0100

    Fix the typo in app.rb

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,8 @@ module CooperApi
     config.api_only = true
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins ‘*’
-        resource ‘*’, headers: :any, methods: [:get, :post, :put, :delete],
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :put, :delete],
                       expose: %w(access-token expiry token-type uid client),
                       max_age: 0
       end
